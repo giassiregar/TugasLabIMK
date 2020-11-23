2557b904f7436bcc55e68682d14395515ec1e0b2
Anggia Sari Siregar 191402033 <66856358+giassiregar@users.noreply.github.com>
2020-11-23 19:37:50 +0700

    laman utama

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <style>
+
+body {background-image: url(latarbiru.png); background-size: 1380px;}
+
+table, th ,td{border: 1px solid black; text-align: center;}
+td{border: 1px solid black;}
+th , td {height: 50px;}
+
+ul {list-style-type: none;margin: 0;padding: 0;overflow: auto;background-color: #333; width: 100%;}
+li {float: left;}
+li a {display: block;color: white;text-align: center;padding: 14px 16px;text-decoration: none;}
+li a:hover:not(.active) {background-color: white;}
+.active {background-color: #4CAF50;}
+
+.content{
+  padding-top: 230px;
+  text-align:center;
+  width: 1350px;
+  height:500px;
+} 
+h1 {color: white;}
+</style>
+</head>
+<body>
+<ul>
+  <a class="navbar-brand" href="#">e-Library</a>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="dataanggota.php">Data Anggota</a></li>
+</ul>
+<div class="content">
+    <h1><b>Selamat Datang di Portal Layanan e-Library</b></h1>
+    <h3>Untuk memenuhi kebutuhan pemustaka, kami melanggan berbagai bahan perpustakaan digital online (e-Resources) seperti jurnal , ebook, dan karya-karya referensi online lainnya. </h3>
+  </div>
+
+</body>
+</html>
