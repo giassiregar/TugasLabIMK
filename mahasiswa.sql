0d0bf19935648ca8bec7ae061fc40ba6350042a5
Anri Marpaung 191402087 <72986928+Yorskies@users.noreply.github.com>
2020-11-23 21:47:19 +0700

    Ubah nama database dan tambah tabel admin

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2020 at 11:42 AM
--- Server version: 10.4.16-MariaDB
--- PHP Version: 7.3.24
+-- Generation Time: Nov 23, 2020 at 03:45 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,8 +19,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `perkuliahan`
+-- Database: `lab_perkuliahan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'anri123', 'anri123');
 
 -- --------------------------------------------------------
 
@@ -39,7 +59,35 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`nim`, `nama`, `jurusan`, `alamat`) VALUES
-(191402033, 'Anggia', 'TI2019', 'jalan selamat125');
+(191402034, 'Anggia Fitriani', 'Ilmu Komputer', 'Jalan Selamat 125'),
+(191402051, 'Yohana Fransisca', 'Teknologi Informasi', 'Ring Road'),
+(191402087, 'Anri Anugerah Marpaung', 'Teknologi Informasi', 'Jln. Pancing No. 69 Medan ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
