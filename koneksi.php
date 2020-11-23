0dc93a7b1f1cd07f22615173dc69c5bb58709508
Anri Marpaung 191402087 <72986928+Yorskies@users.noreply.github.com>
2020-11-23 21:54:02 +0700

    ubah nama database

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$koneksi = mysqli_connect("localhost","root","","perkuliahan");
+$koneksi = mysqli_connect("localhost","root","","lab_perkuliahan");
 
 //check connection
 if (mysqli_connect_errno()){
