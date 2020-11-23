38453724f29360d637a16da252560980b14d069e
Anri Marpaung 191402087 <72986928+Yorskies@users.noreply.github.com>
2020-11-23 21:50:25 +0700

    Ubah index.php

--- index.php
+++ index.php
@@ -1,44 +1,44 @@
+<?php
+session_start();
+error_reporting(0);
+include 'koneksi.php';
+?>
+
 <!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="css/bootstrap.css">
-  <script type="text/javascript" src="js/jquery.js"></script>
-  <script type="text/javascript" src="js/bootstrap.js"></script>
-  <style>
-
-body {background-image: url(latarbiru.png); background-size: 1380px;}
-
-table, th ,td{border: 1px solid black; text-align: center;}
-td{border: 1px solid black;}
-th , td {height: 50px;}
-
-ul {list-style-type: none;margin: 0;padding: 0;overflow: auto;background-color: #333; width: 100%;}
-li {float: left;}
-li a {display: block;color: white;text-align: center;padding: 14px 16px;text-decoration: none;}
-li a:hover:not(.active) {background-color: white;}
-.active {background-color: #4CAF50;}
-
-.content{
-  padding-top: 230px;
-  text-align:center;
-  width: 1350px;
-  height:500px;
-} 
-h1 {color: white;}
-</style>
-</head>
-<body>
-<ul>
-  <a class="navbar-brand" href="#">e-Library</a>
-  <li><a class="active" href="index.php">Home</a></li>
-  <li><a href="dataanggota.php">Data Anggota</a></li>
-</ul>
-<div class="content">
-    <h1><b>Selamat Datang di Portal Layanan e-Library</b></h1>
-    <h3>Untuk memenuhi kebutuhan pemustaka, kami melanggan berbagai bahan perpustakaan digital online (e-Resources) seperti jurnal , ebook, dan karya-karya referensi online lainnya. </h3>
-  </div>
-
-</body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css"> 
+  </head>
+  <body>
+    <form class="box" action="" method="POST">
+      <h1>Login</h1>
+      <input type="text" name="username" placeholder="Username" value="">
+      <input type="password" name="password" placeholder="Password" value="">
+      <input type="submit" name="login" value="Login">
+    </form>
+  </body>
 </html>
+
+<?php
+if(isset($_POST["login"])){
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $ambil = mysqli_query($koneksi, "SELECT * FROM admin WHERE username='$username' AND password='$password'");
+
+  $akunyangcocok = mysqli_num_rows($ambil);
+
+  if($akunyangcocok > 0){
+    $akun = $ambil->fetch_assoc();
+    $_SESSION["admin"] = $akun;
+    echo "<script>alert('Sukses Login!');</script>";
+    echo "<script> location='dataanggota.php';</script>";
+  }
+  else {
+    echo "<script>alert('Gagal Login!');</script>";
+  }
+}
+
+?>
